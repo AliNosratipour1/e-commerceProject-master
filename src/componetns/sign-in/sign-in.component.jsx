ce6e6ecb3def4136b312df5a3f243ebf7585b312
ali <ali008009@yahoo.com>
2020-01-09 13:33:40 +0000

    finishing sign-in and sign-out page

--- src/componetns/sign-in/sign-in.component.jsx
+++ src/componetns/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './sign-in.style.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {signInwithGoogle} from '../../firebase/firebase.utils';
+import {auth,signInwithGoogle} from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -12,8 +12,18 @@ class SignIn extends React.Component {
           password: ''  
          }
     }
-handelSubmit = event =>{
+handelSubmit = async event =>{
     event.preventDefault();
+    const {email,password} = this.state;
+
+try{
+await auth.signInWithEmailAndPassword(email,password);
+this.setState({email:'' , password:''});
+}catch(error){
+  console.log(error);
+
+}
+
     this.setState({email: '', password:''})
 }
 
